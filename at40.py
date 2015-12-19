30649eb3b6f44d76fdfa5d10730094e4c59c8b11
Shubham singh <shubhams805@gmail.com>
2015-12-20 01:30:04 +0530

    Update at40.py

--- at40.py
+++ at40.py
@@ -1,3 +1,6 @@
+''' the user should enter the year and month and the script will display the top 3 songs of that month '''
+
+
 from bs4 import BeautifulSoup
 import requests
 month=['NAN','JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
@@ -5,15 +8,17 @@ year = str(input("enter the year"))
 mon = str(input("enter the month"))
 mon=mon.upper()
 
-
 mon=month.index(mon)
 req = requests.get("http://www.at40.com/top-40/" + year +'/'+ str(mon))
 content = req.text
 code= BeautifulSoup(content)
 store = code.find('td',"listtable1")
 print("\n\n")
+
 data=str(store.get_text())
+
 data=' '.join(data.split())
+
 print(data)
 
 
