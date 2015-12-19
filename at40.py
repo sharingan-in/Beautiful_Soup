c86fd75163b5a49df9ca018c43c2472891d0baae
Shubham <shubhams805@gmail.com>
2015-12-20 01:24:03 +0530

    top songs from at40

---
+++ at40.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+month=['NAN','JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC']
+year = str(input("enter the year"))
+mon = str(input("enter the month"))
+mon=mon.upper()
+
+
+mon=month.index(mon)
+req = requests.get("http://www.at40.com/top-40/" + year +'/'+ str(mon))
+content = req.text
+code= BeautifulSoup(content)
+store = code.find('td',"listtable1")
+print("\n\n")
+data=str(store.get_text())
+data=' '.join(data.split())
+print(data)
+
+
